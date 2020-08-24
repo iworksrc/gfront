07ea9d72313f6ab573f09dc7397ab9aa848c400a
rke <imail.for.iwork@gmail.com>
2020-08-24 13:16:26 +0700

    add modules: shared, main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,24 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { SharedModule } from './modules/shared/shared.module';
+import { MainModule } from './modules/main/main.module';
+import {Route, RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: () => import('./modules/main/main.module').then(mod => mod.MainModule)
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+];
+
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +30,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    SharedModule,
+    MainModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/main/main.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent }
+];
+
+@NgModule({
+  declarations: [MainComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class MainModule { }
